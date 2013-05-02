97d0f9bfba817d02fb5bb8b1e53b433e33abcb1f
pegelston <pegelston@gmail.com>
2013-05-02 17:27:00 -0500

    Influence grid mapping project

---
+++ AIFinalProject/src/Cell.java
@@ -0,0 +1,39 @@
+import processing.core.PApplet;
+
+public class Cell
+{
+	// A cell object knows about its location in the grid as well as its size with the variables x,y,w,h.
+	float x,y;   // x,y location
+	float w,h;   // width and height
+	int color;
+	float influence;
+	PApplet parent; // The parent PApplet that we will render ourselves onto
+	
+	// Cell Constructor
+	Cell(float tempX, float tempY, float tempW, float tempH, PApplet p) 
+	{
+	    x = tempX;
+	    y = tempY;
+	    w = tempW;
+	    h = tempH;
+	    color = 127;
+	    influence = 0.0f;
+	    parent = p;
+	}
+			  	
+	void display()
+	{
+		if(influence < 0.0)
+		{
+			influence = 0.0f;
+		}
+//		if(influence > 1.0)
+//		{
+//			influence = 1.0f;
+//		}
+
+		parent.stroke(100);
+		parent.fill(parent.color(255*influence, 0, 0));
+		parent.rect(x,y,w,h);
+	}
+}
---
+++ AIFinalProject/src/Influence.java
@@ -0,0 +1,57 @@
+import processing.core.PApplet;
+
+public class Influence 
+{
+	PApplet parent;
+	Grid grid;
+	
+	float momentum;
+	float influence = 1.0f;
+	int delta = 0;
+	int area = 1;
+	
+	int c_index;
+	int r_index;
+	
+	Influence(PApplet p, Grid _grid, int c, int r)
+	{
+		parent = p;
+		grid = _grid;
+		c_index = c;
+		r_index = r;
+		
+		momentum = 0.025f;
+	}
+	
+	void propogateInfluence()
+	{
+		for(int runner = 0; runner < area; runner++)
+		{
+			if(c_index-delta >= 0 && r_index-delta+runner >= 0 && r_index-delta+runner < grid.rows)
+			{
+				grid.grid[c_index-delta][r_index-delta+runner].influence += influence;
+			}
+			if(r_index+delta-runner >= 0 && r_index+delta-runner < grid.rows && c_index+delta < grid.columns)
+			{
+				grid.grid[c_index+delta][r_index+delta-runner].influence += influence;
+			}
+		}
+		
+		for(int runner = 1; runner < area - 1; runner++)
+		{
+			if(c_index-delta+runner >= 0 && c_index-delta+runner < grid.columns && r_index-delta >= 0)
+			{
+				grid.grid[c_index-delta+runner][r_index-delta].influence += influence;
+			}
+			
+			if(c_index+delta-runner >= 0 && c_index+delta-runner < grid.columns && r_index+delta < grid.rows)
+			{
+				grid.grid[c_index+delta-runner][r_index+delta].influence += influence;
+			}			
+		}
+		
+		delta++;
+		area += 2;
+		influence -= momentum;
+	}
+}
